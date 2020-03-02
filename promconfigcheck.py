7045b2e4d626e442c792c99d0b46c1375fd72ece
Dr. William N Roney <33969189+wroney688@users.noreply.github.com>
2020-03-02 12:03:44 -0500

    Enhanced for non-keyed XML

--- promconfigcheck.py
+++ promconfigcheck.py
@@ -1,7 +1,7 @@
 from flask import Flask, Response, request
 from prometheus_client import generate_latest, Info, Gauge, Histogram, Counter, Summary, CONTENT_TYPE_LATEST, start_http_server
 from prometheus_flask_exporter import PrometheusMetrics
-import time, subprocess, yaml, re, glob, getopt, sys, threading, atexit
+import re, glob, getopt, sys
 import xml.etree.ElementTree as etree
 
 METRIC_PREFIX = 'configcheck'
@@ -35,29 +35,64 @@ def formattedAttributes(myAttr):
 def formattedValue(myVal):
     fStr = ''
     if myVal is not None and myVal.strip() != "":
-        fStr = '({0})'.format(myVal.strip())
+        fStr = '{0}'.format(myVal.strip())
     return fStr
 
-def recursiveFlatten(xml):
+def hasDupNodes(xml):
+    retval = False
+    dupCounts = {}
+    children = [removeNamespace(i.tag) for i in list(xml)]
+    distinctChildren = [*set(children),]
+    countChildren = len(children)
+    countDistinctChildren = len(distinctChildren)
+    if(countChildren != countDistinctChildren):
+        retval = True
+    dupList = [len([x for x in children if x == i]) for i in distinctChildren]
+    if len(children) > 0:
+        for i in range(0,countDistinctChildren):
+            if dupList[i] > 1:
+                dupCounts[distinctChildren[i]] = dupList[i]
+    return retval, dupCounts
+
+def recursiveFlatten(xml, duparent = False, depth=0):
     children = list(xml)
     leaves = []
     subleaves = []
     myVal = xml.text
     myAttribs = []
+
+    tabs = ''
+    for x in range(0, depth): tabs = tabs + '\t'
+
+    hasDups, dupTags = hasDupNodes(xml)
+
     if xml.text is not None: #clean up whitespace
         myVal = xml.text.strip()
     for n, v in xml.attrib.items():
         cleanname = removeNamespace(n)
         if cleanname != 'schemaLocation': # intentionally omit this tag
             myAttribs.append('{0}={1}'.format(cleanname, v))
-    prefix = '{0}{1}'.format(removeNamespace(xml.tag), formattedValue(myVal))
+
+    prefix = '{0}{1}.'.format(removeNamespace(xml.tag), formattedValue(myVal))
+
     if len(children) == 0:
-        return ['{0}{1}{2}'.format(removeNamespace(xml.tag), formattedAttributes(myAttribs), formattedValue(myVal))]
+        leaves.append('{0}{1}={2}'.format(removeNamespace(xml.tag), formattedAttributes(myAttribs), formattedValue(myVal)))
     else:
         for child in children:
-            subleaves.append(', '.join(recursiveFlatten(child)))
-    for leaf in subleaves: #prepend my prefix
-        leaves.append('{0}{1}.{2}'.format(prefix, formattedAttributes(myAttribs), leaf))
+            inDups = False
+            if removeNamespace(child.tag) in dupTags:
+                inDups = True
+            sl = recursiveFlatten(child, inDups, depth+1)
+            if inDups == False:
+                for l in sl:
+                    subleaves.append(l)
+            else:
+                subleaves.append(formattedAttributes(sl))
+        if len(subleaves) == 0:
+            leaves.append('{0}{1}'.format(prefix, formattedAttributes(myAttribs)))
+        else:
+            for leaf in subleaves: #prepend my prefix
+                leaves.append('{0}{1}{2}'.format(prefix, formattedAttributes(myAttribs), leaf))
     return leaves
 
 def compareXML(configName, model, actual):
@@ -98,3 +133,4 @@ if __name__ == '__main__':
 
     print('OS UID: {0}'.format(userID))
     app.run(debug=True, host='0.0.0.0', port=PORT)
+
